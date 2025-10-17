b760f2c0871888acc1b854b5dda73aee6054eb29
oveljosland <oveljosland@icloud.com>
2025-10-17 13:44:43 +0200

    add signals

--- src/clk.vhd
+++ src/clk.vhd
@@ -4,6 +4,10 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity clkd is
+	generic (
+		constant NUM_BIT: positive := 4;
+		constant CLK_FRQ: positive := 50; /* MHz */
+		constant SCK_PER: positive := 20; /* scaled clk period in ns */
 	port (
 		clk: in std_logic; /* system clock */
 		rst: in std_logic; /* system reset */
@@ -11,3 +15,8 @@ entity clkd is
 	);
 end entity;
 
+architechture rtl of clkd is
+	constant CLK_DIV: positive := 0;
+	signal sck: std_logic := '0'; /* scaled clock */
+	signal cnt: natural range from 0 to M - 1 := '0';
+end architechture;
