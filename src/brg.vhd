6cb1995a79e427142ff2a7b59f418a184923d0fd
oveljosland <oveljosland@icloud.com>
2025-10-17 14:48:19 +0200

    fix if/elsif

--- src/brg.vhd
+++ src/brg.vhd
@@ -8,7 +8,6 @@ entity brg is
 	generic (
 		constant B: positive := 9600; /* baud rate */
 		constant CLK_FRQ: positive := 50; /* MHz */
-		constant SCK_PER: positive := 20 /* scaled clk period in ns */
 	);
 	port (
 		clk: in std_logic; /* system clock */
@@ -18,15 +17,15 @@ entity brg is
 end entity;
 
 architecture rtl of brg is
-	constant M: positive := CLK_FRQ / (B * 1000); /* scaler */
+	constant M: positive := CLK_FRQ / (B * 1000); /* roof */
 	signal i: natural range from 0 to M - 1 := 0;
 	signal r: std_logic := '0'; /* hold output clock */
 begin
 	gen: process(clk, rst) begin
-		if rst = '0' then
+		if rst = '1' then
 			i <= 0;
 			r <= '0';
-		else rising_edge(clk) then
+		elsif rising_edge(clk) then
 			if i = M - 1 then
 				i <= 0;
 				r <= not r;
