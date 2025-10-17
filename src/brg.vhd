857bbd9d48c2fa5fbfe74272e6386ef38ae88acb
oveljosland <oveljosland@icloud.com>
2025-10-17 14:37:10 +0200

    restructure

--- src/brg.vhd
+++ src/brg.vhd
@@ -1,37 +1,38 @@
-/* vhdl 2008 */
+/* baud rate generator */
 
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.std_numeric.all;
 
-entity clkd is
+entity brg is
 	generic (
-		constant NUM_BIT: positive := 4;
+		constant BDR: positive := 9600; /* baud rate */
 		constant CLK_FRQ: positive := 50; /* MHz */
 		constant SCK_PER: positive := 20; /* scaled clk period in ns */
 	port (
 		clk: in std_logic; /* system clock */
 		rst: in std_logic; /* system reset */
-		sck: out std_logic
+		bck: out std_logic /* generated clock */
 	);
 end entity;
 
-architecture rtl of clkd is
-	constant M: positive := CLK_FRQ * SCK_PER / 1000;  /* scaler */
-	signal sck: std_logic := '0';  /* scaled clock */
+architecture rtl of brg is
+	constant M: positive := BRD * CLK_FRQ / 1000; /* scaler */
 	signal cnt: natural range from 0 to M - 1 := '0';
+	signal bck: std_logic := '0';  /* 'baud' clock */
 begin
-	/* presc:  prescale clock */
-	presc: process(clk, rst) begin
+	gen: process(clk, rst) begin
 		if rst = '0' then
 			cnt <= 0;
-			sck <= '0';
+			bck <= '0';
 		else rising_edge(clk) then
 			if cnt = M - 1 then
 				cnt <= 0;
-				sck <= not sck;
+				bck <= not bck;
 			else
 				cnt <= cnt + 1;
 			end if;
 		end if;
 	end process;
+	bck <= bck;
 end architecture;
