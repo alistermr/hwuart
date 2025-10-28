136f9b7e0a2fdb478d6da6c854c765dca57858b9
oveljosland <oveljosland@icloud.com>
2025-10-28 15:48:08 +0100

    add requirements

--- src/urx.vhd
+++ src/urx.vhd
@@ -1,4 +1,19 @@
-/* uart rx */
+/* requirements */
+-- must support the UART protocol.
+	-- 1 start bit.
+	-- 8 data bits.
+	-- 1 stop bit.
+	-- no parity bits.
+-- must support baud rate of at least 9600.
+-- must be able to store at least one received byte.
+-- must use 8x oversampling on the rx signal, and sample the middle of the
+-- bit period to determine the value.
+-- must indicate when data is received and ready to be used (signal).
+-- can do majority decision based on 5 samples in the middle of the bit period.
+-- can have a 16 bit FIFO to store bytes, delete new data when full.
+-- should support parity control (even, odd, none).
+-- should be able to change baud rate when running.
+
 
 library ieee;
 use ieee.std_logic_1164.all;
