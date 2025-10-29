cfcfa522684d853ff1d12ee54fbcdd70733daad5
oveljosland <oveljosland@icloud.com>
2025-10-29 12:22:59 +0100

    add signals and constants

--- src/utx.vhd
+++ src/utx.vhd
@@ -16,13 +16,20 @@ use ieee.numeric_std.all;
 
 entity utx is
 	generic (
-
+		constant BITWIDTH: positive := 8;
+		constant SYS_CLK_FRQ: positive := 50; /* MHz */
+		constant BAUD_RATE: positive := 9600; /* B/s */
 	);
 	port (
-
+		clk: in std_logic; /* system clock */
+		bi: out std_logic /* byte in */
+		dv: out std_logic; /* data valid */
+		so: in std_logic; /* serial out */
 	);
 end entity;
 
 architecture rtl of utx is
+	type state is (idle, startbit, databit, stopbit, flush);
+	signal s: state := idle;	
 begin
 end architecture;
